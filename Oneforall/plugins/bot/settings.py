d52ff5e69b4988997c2f04dffd62a526bbb6bf42
vagetatg <nibbanmisal3302@gmail.com>
2025-10-18 02:01:17 +0530

    Refactor settings.py with updated callback handling

--- Oneforall/plugins/bot/settings.py
+++ Oneforall/plugins/bot/settings.py
@@ -55,7 +55,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://files.catbox.moe/8qigce.mp4"),
+        InputMediaVideo("https://files.catbox.moe/htfqe1.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
@@ -406,3 +406,4 @@ async def vote_change(client, CallbackQuery, _):
         )
     except MessageNotModified:
         return
+
